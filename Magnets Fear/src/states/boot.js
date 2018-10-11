f3a081de6a22f1959f49f4dd5a127ac2d56240ed
Pistoncito <victorglezrivera@hotmail.com>
2018-10-11 22:07:30 +0200

    Update boot.js

--- Magnets Fear/src/states/boot.js
+++ Magnets Fear/src/states/boot.js
@@ -6,12 +6,7 @@ MagnetsFear.bootState = function(game) {
 }
 
 //Programacion orientada a datos
-//no instance
-MagnetsFear.spriteStruct= function(string)
-{
-    this.address= string;
-    this.data;
-}
+
 
 MagnetsFear.control_manager= function(x,y){
 
@@ -29,37 +24,33 @@ MagnetsFear.control_manager= function(x,y){
    this.Abool=false;
    this.Sbool=false;
    this.Dbool=false;
-   this.Control= function(key, size)
+   this.Control= function(key,sprite)
    {
-    var keys_left= size;   
-    for(i=0; i< size; i++)
-    {
-        switch(key[i])
+        alert("wornk");
+        switch(key)
         {
          case (this.Wkey):
-                  this.y -= 100;
-                  //alert(this.y);
+                this.y -= 100;
+                alert("W");
          break;
  
          case (this.Akey):
               this.x -= 100
-            // alert(this.x);
+              alert("A");
          break;
  
          case (this.Skey):
              this.y += 100
-              //alert(this.y);
+             alert("S");
          break;
  
          case (this.Dkey):
              this.x += 100
-             //alert(this.x);
+             alert("D");
          break;
         }
-
-
-    }
-      
+        sprite.x = this.x;
+        sprite.y = this.y;
 
    }
 
@@ -68,9 +59,44 @@ MagnetsFear.gameObject= function(x,y,sprite_size, control){
     
     this.n_sprites= sprite_size
     this.sprites= new Array(this.n_sprites);
+    this.currentSprite=0;
+
     this.control_manager=null;
     if(control)
     this.control_manager= new MagnetsFear.control_manager(x,y);
+
+    this.sayHello= function(){ alert("hello")}
+    this.Control= function(Key)
+    {
+        if(this.control_manager !=null)
+        {
+           
+            switch(Key)
+            {
+             case (this.Wkey):
+                    this.y -= 100;
+                    alert("W");
+             break;
+     
+             case (this.Akey):
+                  this.x -= 100
+                  alert("A");
+             break;
+     
+             case (this.Skey):
+                 this.y += 100
+                 alert("S");
+             break;
+     
+             case (this.Dkey):
+                 this.x += 100
+                 alert("D");
+             break;
+            }
+            this.sprites[this.currentSprite].x= this.control_manager.x;
+            this.sprites[this.currentSprite].y= this.control_manager.y;
+        }
+    }
    
  }
 
