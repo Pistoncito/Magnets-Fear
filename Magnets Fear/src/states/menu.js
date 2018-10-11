3f7586ebb3e131492ab322e63f5749e00d393902
Pistoncito <victorglezrivera@hotmail.com>
2018-10-11 20:45:53 +0200

    Update menu.js

--- Magnets Fear/src/states/menu.js
+++ Magnets Fear/src/states/menu.js
@@ -1,11 +1,22 @@
 MagnetsFear.menuState = function(game) {
 
 }
-
+ var options;
 MagnetsFear.menuState.prototype = {
 
     preload: function() {
-    	    
+        var style= {fill:"rgb(0,90,120)", font:"60px"};
+        //jugar, opciones, salir
+       options=["Jugar", "Opciones", "Salir"];
+   
+        var x=game.canvas.width/2 -300;
+        var y=game.canvas.height/2- 3*(60+20);
+        var yOffset= 60 +20;
+        for(var i= 0; i<options.length; i++)
+            {
+                game.add.text(x, y, options[i], style);
+                y+= yOffset;
+            }
     },
     
     create: function() {
@@ -13,7 +24,7 @@ MagnetsFear.menuState.prototype = {
     },
 
     update: function() {
-   
+   game.state.start('levelState');
     	
     }
 }
