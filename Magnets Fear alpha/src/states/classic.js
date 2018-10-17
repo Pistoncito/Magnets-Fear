97bd8b4a029a5ab1b92a8334bb198e573ccad78e
Álvaro Ramírez <43178036+AvisPraeda@users.noreply.github.com>
2018-10-17 16:28:01 +0200

    Add files via upload

---
+++ Magnets Fear alpha/src/states/classic.js
@@ -0,0 +1,124 @@
+MagnetsFear.classicState = function(game) {
+}
+
+//Variables
+var bg;
+var numPlayers = 1;
+var numProyectiles = 4;
+var numPlayerBases = 3;
+var possible_keys = [
+  Phaser.Keyboard.W,
+  Phaser.Keyboard.A,
+  Phaser.Keyboard.S,
+  Phaser.Keyboard.D];
+
+
+
+
+//FUNCIONES
+
+//Detección de Colisiones entre círculos
+function collidesCircleCircle(body1, body2){
+  var radius1 = body1.width * 0.5;
+  var radius2 = body2.width * 0.5;
+  var distance = getPowDistance(body1.x, body1.y, body2.x, body2.y);
+  if (distance <= (radius1 + radius2)*(radius1 + radius2)){
+    return true;
+
+  }  
+  return false;
+};
+//Distancia entre centros
+function getDistance(fromX, fromY, toX, toY){
+  var a = Math.abs(fromX - toX);
+  var b = Math.abs(fromY - toY);
+  return Math.sqrt((a * a) + (b * b));
+};
+
+function getPowDistance(fromX, fromY, toX, toY){
+  var a = Math.abs(fromX - toX);
+  var b = Math.abs(fromY - toY);
+  return Math.abs((a * a) + (b * b));
+};
+
+function proyectileHitsPlayer(player,proyectile){
+  proyectile.sprite.animations.play('proyect2');
+} 
+
+MagnetsFear.classicState.prototype = {
+
+    preload: function() {
+        //prevents possible keys from propagating to the browser
+        game.input.keyboard.addKeyCapture(this.possible_keys);
+        /*
+        this.wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        this.sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+        this.aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+        this.dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        
+        for(i=0; i< this.keysPressed.length;i++)
+            {
+                this.keysPressed[i]= game.input.keyboard.addKey(this.possible_keys[i]);
+            }
+        */
+    },
+
+    create: function() {
+     
+        bg = game.add.image(0,0,'background');
+
+        
+        // Activa eventos de impacto
+        //game.physics.p2.setImpactEvents(true);
+        game.physics.p2.restitution = 1.0;
+        // Se crean los grupos de colisiones
+        var playerCollisionGroup = game.add.physicsGroup(Phaser.Physics.P2JS);
+        var proyectilesCollisionGroup = game.add.physicsGroup(Phaser.Physics.P2JS);
+        // Necesario para que los objetos colisionen contra los bordes
+        game.physics.p2.updateBoundsCollisionGroup();
+
+        //Creación del grupo de proyectiles
+        var proyectiles = game.add.group();
+        proyectiles.enableBody = true;
+        proyectiles.physicsBodyType = Phaser.Physics.P2JS;
+        for (var i = 0; i < numProyectiles; i++)
+        {
+          var proyectil = proyectiles.create(game.world.randomX, game.rnd.between(0, 100), 'proyectile1');
+          proyectil.body.setCircle(30);
+          proyectil.body.fixedRotation = true;          
+          proyectil.animations.add('proyect1',[0],60,true);
+          proyectil.animations.add('proyect2',[0],60,true);
+          proyectil.body.velocity.x = 200;
+          proyectil.body.velocity.y = -200;
+          proyectil.body.damping = 0;
+          // Se le asigna a los proyectiles su grupo de Colisiones
+          proyectil.body.setCollisionGroup(proyectilesCollisionGroup);
+          // Proyectiles colisionan contra otros proyectiles y contra las esferas
+          //proyectil.body.collides([proyectilesCollisionGroup, playerCollisionGroup]);
+        }
+        //Creación del grupo de esferas(jugadores)
+        var esferas = game.add.group();
+        esferas.enableBody = true;
+        esferas.physicsBodyType = Phaser.Physics.P2JS;
+        for (var i = 0; i < numPlayers; i++)
+        {
+          var esfera = esferas.create(game.world.randomX, game.rnd.between(0, 100), 'sphere1p');
+          esfera.body.setCircle(45);
+          esfera.body.fixedRotation = true;
+          esfera.body.damping = 0;
+          // Se le asigna a las esferas su grupo de colisiones
+          esfera.body.setCollisionGroup(playerCollisionGroup);
+          // Si la esfera choca contra un proyectil llama a la función hitProyectil
+          //esfera.body.collides([proyectilesCollisionGroup, playerCollisionGroup]);
+        }        
+      },
+
+
+
+    update: function() {
+
+    },
+    
+
+
+}
