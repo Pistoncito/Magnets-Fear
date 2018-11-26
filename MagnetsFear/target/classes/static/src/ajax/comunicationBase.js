8f72d76bbf3b7ee383165168a7b70268d2d4e3e2
Pistoncito <victorglezrivera@hotmail.com>
2018-11-26 02:50:51 +0100

    Comunicacion con el servidor de bases

---
+++ MagnetsFear/target/classes/static/src/ajax/comunicationBase.js
@@ -0,0 +1,53 @@
+
+
+function getNumberBases(callback)
+{
+    $.ajax(
+        {
+            method:"GET",
+            url: "http://localhost:8080/bases",
+            processData: false,
+            header:{
+                "Content-Type": "application/json"
+            }
+            
+        }).done(function(nBases){
+            console.log(JSON.stringify(nBases) + " is the Number of bases");
+            callback(nBases);
+        });
+
+}
+
+function createBases(callback)
+{
+    $.ajax({
+        method:"POST",
+        url:"http://localhost:8080/bases",
+        processData: false,
+        header:
+        {
+            "Content-Type": "application/json"
+        }
+    }).done(function(nBases){
+        console.log(JSON.stringify(nBases) +" bases created");
+        callback(nBases);
+    });
+}
+
+function getBase(callback,id)
+{
+    $.ajax({
+        method:"GET",
+        url: "http://localhost:8080/bases/" + id,
+        processData:false,
+        header: 
+        {
+            "Content-Type": "application/json"
+        }
+
+    }).done(function(base){
+    console.log("Get base: " + JSON.stringify(base));
+        callback(base);
+    })
+
+}
