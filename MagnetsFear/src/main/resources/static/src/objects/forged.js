848d311a1c766036d70dabbbef05d65b99b2bb12
Álvaro Ramírez <43178036+AvisPraeda@users.noreply.github.com>
2018-12-02 01:23:12 +0100

    Delete forged.js

--- MagnetsFear/src/main/resources/static/src/objects/forged.js
+++
@@ -1,9 +0,0 @@
-function Forged(x, y, sprite) {
-    this.sprite = game.add.sprite(x, y, sprite);
-    this.sprite.anchor.setTo(0.5, 0.5);
-    
-    this.destroy = function(){
-        this.sprite.destroy();
-        delete this;
-    }
-}
