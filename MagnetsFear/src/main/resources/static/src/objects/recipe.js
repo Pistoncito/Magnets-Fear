a7dbfd2101e81179fc786a910332535e05a31a6c
Álvaro Ramírez <43178036+AvisPraeda@users.noreply.github.com>
2018-12-02 01:22:57 +0100

    Delete recipe.js

--- MagnetsFear/src/main/resources/static/src/objects/recipe.js
+++
@@ -1,9 +0,0 @@
-function Recipe(x, y, sprite) {
-    this.sprite = game.add.sprite(x, y, sprite);
-    this.sprite.anchor.setTo(0.5, 0.5);
-    
-    this.destroy = function(){
-        this.sprite.destroy();
-        delete this;
-    }
-}
