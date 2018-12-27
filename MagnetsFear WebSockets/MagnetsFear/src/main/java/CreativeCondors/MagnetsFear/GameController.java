00de9f3815f2a68ebcce3e7f4af153b80d42cecf
Álvaro Ramírez <43178036+AvisPraeda@users.noreply.github.com>
2018-12-27 19:04:09 +0100

    Comentarios

--- MagnetsFear WebSockets/MagnetsFear/src/main/java/CreativeCondors/MagnetsFear/GameController.java
+++ MagnetsFear WebSockets/MagnetsFear/src/main/java/CreativeCondors/MagnetsFear/GameController.java
@@ -18,34 +18,42 @@ public class GameController {
 	int maxProyectiles = 2;
 	Random rnd = new Random();
 
+	// Devuelve el número de jugadores
 	public int numPlayers() {
 		return players.size();
 	}
-	
+
+	// Devuelve el número de proyectiles
 	public int numProyectiles() {
 		return proyectiles.size();
 	}
-		
+
+	// Devuelve el número máximo de bases por jugador
 	public int getMaxBases() {
 		return maxBasesPerPlayer;
 	}
 
+	// Devuelve el nº máx. de proyectiles
 	public int getMaxProyectiles() {
 		return maxProyectiles;
 	}
 
+	// Devuelve la colección de jugadores
 	public Collection<Player> getPlayers() {
 		return players.values();
 	}
 
+	// Devuelve la colección de bases
 	public Collection<Base> getBases(Player p) {
 		return p.getBasesPlayer();
 	}
 
+	// Devuelve la colección de proyectiles
 	public Collection<Proyectile> getProyectiles() {
 		return proyectiles.values();
 	}
 
+	// Recibe coordenadas de posición y crea un jugador
 	public Player newPlayer(float x, float y) {
 		Player player = new Player();
 		long id = nextPlayerId.incrementAndGet();
@@ -56,11 +64,13 @@ public class GameController {
 		return player;
 	}
 
+	// Recibe coordenadas de posición y un jugador y crea una base para ese jugador
 	public Base newBase(Player p, float x, float y) {
 		Player player = players.get(p.getId());
 		return player.newBasePlayer(x, y);
 	}
 
+	// Crea un nuevo proyectil
 	public Proyectile newProyectile() {
 		Proyectile proyectile = new Proyectile();
 		long id = nextProyectileId.incrementAndGet();
@@ -69,6 +79,7 @@ public class GameController {
 		return proyectile;
 	}
 
+	// Borra un jugador dado un identificador
 	public void deletePlayer(long id) {
 		Player savedPlayer = players.get(id);
 		if (savedPlayer != null) {
@@ -76,18 +87,22 @@ public class GameController {
 		}
 	}
 
+	// Borra una base de un jugador dado un identificador
 	public void deleteBase(Player p, long id) {
 		Player player = players.get(p.getId());
 		player.deleteBasePlayer(id);
 	}
 
+	// Borra un proyectil dado un identificador
 	public void deleteProyectile(long id) {
 		Proyectile proyectile = proyectiles.get(id);
-		if(proyectile != null) {
+		if (proyectile != null) {
 			proyectiles.remove(id);
 		}
 	}
-	
+
+	// Recibe un jugador
+	// Actualiza los valores del jugador
 	public boolean updatePlayer(Player p) {
 		Player savedPlayer = players.get(p.getId());
 		if (savedPlayer != null) {
@@ -100,6 +115,8 @@ public class GameController {
 		return false;
 	}
 
+	// Recibe una base y un id del jugador
+	// Actualiza los valores de la base en el servidor
 	public boolean updateBase(long playerId, Base b) {
 		Player p = players.get(playerId);
 		if (p != null) {
@@ -109,7 +126,8 @@ public class GameController {
 			return false;
 		}
 	}
-
+	// Recibe un proyectil
+	// Actualiza los valores de ese proyectil en el servidor
 	public boolean updateProyectile(Proyectile p) {
 		Proyectile savedproy = proyectiles.get(p.getId());
 		if (savedproy != null) {
@@ -120,7 +138,8 @@ public class GameController {
 		}
 		return false;
 	}
-
+	// Devuelve un objeto dado un id
+	
 	public Player getPlayer(long id) {
 		return players.get(id);
 	}
@@ -128,12 +147,13 @@ public class GameController {
 	public Proyectile getProyectile(long id) {
 		return proyectiles.get(id);
 	}
-
+	
+	// Devuelve la cantidad de bases que tiene un jugador
 	public int getBasesSize(Player p) {
 		Player player = players.get(p.getId());
 		return player.getBasesSizePlayer();
 	}
-	
+	// Borra los datos del juego
 	public void clearGame()
 	{
 		for(Player p: players.values())
@@ -146,5 +166,4 @@ public class GameController {
 		this.nextPlayerId = new AtomicLong(-1);
 		this.nextProyectileId = new AtomicLong(-1);
 	}
-	
 }
